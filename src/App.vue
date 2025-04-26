18f7e8ce91d9d8adf5cdfcbf7299a35b657155dc
Raimbault <daosuijun@outlook.com>
2025-04-26 17:56:07 +0800

    修正社交链接中的URL，更新至最新地址

--- src/App.vue
+++ src/App.vue
@@ -109,7 +109,7 @@ onMounted(() => {
   const styleTitle1 = "font-size: 20px;font-weight: 600;color: rgb(244,167,89);";
   const styleTitle2 = "font-size:12px;color: rgb(244,167,89);";
   const styleContent = "color: rgb(30,152,255);";
-  const title1 = "Renoの主页\n\n";
+  const title1 = "Renoの主页\n  ";
   const title2 = `
  ██▀███  ▓█████  ███▄    █  ▒█████  
 ▓██ ▒ ██▒▓█   ▀  ██ ▀█   █ ▒██▒  ██▒
@@ -119,8 +119,9 @@ onMounted(() => {
 ░ ▒▓ ░▒▓░░░ ▒░ ░░ ▒░   ▒ ▒ ░ ▒░▒░▒░ 
   ░▒ ░ ▒░ ░ ░  ░░ ░░   ░ ▒░  ░ ▒ ▒░ 
   ░░   ░    ░      ░   ░ ░ ░ ░ ░ ▒  
-   ░        ░  ░         ░     ░ ░ `;
-  const content = `\n\n版本: ${config.version}\n主页: ${config.home}\nGithub: ${config.github}`;
+   ░        ░  ░         ░     ░ ░ 
+   `;
+  const content = `\n版本: ${config.version}\n主页: ${config.home}`;
   console.info(`%c${title1} %c${title2} %c${content}`, styleTitle1, styleTitle2, styleContent);
 });
 
